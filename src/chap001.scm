ab77dd692a6279b9849b4d231b94d8b49f196f87
Stefan Eng <eng_steff@hotmail.com>
2018-12-03 22:15:33 +0100

    Continue in book (@ Ex 1.2).

--- src/chap001.scm
+++ src/chap001.scm
@@ -45,3 +45,96 @@
 ;   1.) Evaluate the subexpression of the combination.
 ;   2.) Apply the value of leftmost sub-expression (operator) to the other
 ;       values of the other sub-expressions (the operands).
+
+; special forms.
+; --------------
+;   Exceptions to the general evaluation rules, ex. (define x 3)
+;   each has its own special evaluation rule.
+
+; General form of a procedure definition is:
+; (define (<name> <formal parameters>) <body>)
+
+(define (square x) (* x x))
+(print (square 21))
+(print (square (+ 2 5)))
+(print (square (square 3)))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+(print (sum-of-squares 3 4))
+
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
+
+(print (f 5))
+
+; 1.1.5 The Substitution Model for Procedure Application
+; ------------------------------------------------------
+; Evaluate compound procedure -> interpreter evaluates the elements of the
+; combination and applies the procedure to the arguments. (value of the
+; operator of the combination to the value of the operands of the combination)
+; Substitute one thing at the time -> substitution model.
+; (Not really how the interpreter works, does not manipulate text!)
+
+; Applicative order versus normal order
+; -------------------------------------
+; Normal-order-eval: Fully expand -> reduce.
+; Applicative-order-eval: evaluate arguments -> apply.
+; Lisp uses Applicative, ex. no need to redo calculation.
+
+; 1.1.6 Conditional Expression and Predicates
+; -------------------------------------------
+; case analysis:
+
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+
+(print (abs -10))
+
+; predicate -> evaluate to True/False.
+
+(define (abs x)
+  (cond ((< x 0) (- x))
+        (else x)))
+
+(print (abs -10))
+
+(define (abs x)
+  (if (< x 0)
+      (- x)
+      x))
+
+(print (abs -10))
+
+; Special form of if with two outcomes.
+; (if <predicate> <consequent> <alternative>)
+; also logical composition operations {and,or,not} (and/or are special forms
+; not procedures, all sub-expressions are not always evaluated)
+
+; Exercise 1.1
+; ------------
+; 10 -> 10
+; (+ 5 3 4) -> 12
+; (- 9 1) -> 8
+; (/ 6 2) -> 3
+; (+ (* 2 4) (- 4 6)) -> 6
+(define a 3) ;-> Value: a
+(define b (+ a 1 )) ;-> Value b
+; (+ a b (* a b)) -> 19
+; (= a b) -> False
+(if (and (> b a) (< b (* a b)))
+  (print b)
+  (print a)) ; -> 4
+(cond ((= a 4) (print 6))
+      ((= b 4) (print (+ 6 7 a)))
+      (else 35)) ; -> 16
+; ( + 2 (if (> b a) b a)) -> 6
+(print (* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))) ; -> 16
+
+; Exercise 1.2
+; ------------
